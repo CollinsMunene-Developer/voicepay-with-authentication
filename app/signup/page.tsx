2768bc244bdc304b660aaeb366b43e1a2edab865
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-11-13 11:06:43 +0000

    first commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,17 +1,57 @@
-import React from 'react'
-import '@/app/globals.css'
-import { ModeToggle } from '../components/toggle-theme-change'
+import "@/app/globals.css";
+import { FaGithub, FaGoogle, FaFacebook, FaApple } from "react-icons/fa";
+import Image from "next/image";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-        <h1 className='text-3xl font-bold underline text-black'>Hello world!</h1>
-      
+    <div className="signupPage bg-slate-400">
+      <div className="signupContainer p-28">
+        <div className="signup_upper">
+          <div className="signupText">
+            <h1>Create an Account</h1>
+            <p>Sign up to start your journey with us</p>
+          </div>
+          <div className="signupButtons">
+            <button className="signupButton">Sign Up</button>
+            <button className="loginButton">Login</button>
+          </div>
+        </div>
 
+        <div className="signup_lower">
+          <div className="signupForm"></div>
 
-      <ModeToggle />
+          <div className="continue_with_Buttons">
+            <p style={{ color: "black", fontSize: "", marginLeft: "8.5rem" }}>Or</p>
+            <button className="continue_with_google">
+              <FaGoogle style={{ height: "20px", width: "20px" }} />
+              Continue with Google
+            </button>
+            <button className="continue_with_github">
+              <FaGithub style={{ height: "24px", width: "24px" }} />
+              Continue with Github
+            </button>
+            <button className="continue_with_facebook" style={{ background: "blue" }}>
+              <FaFacebook style={{ height: "24px", width: "24px" }} />
+              Continue with Facebook
+            </button>
+            <button className="continue_with_apple">
+              <FaApple style={{ height: "24px", width: "24px" }} />
+              Continue with Apple
+            </button>
+            <button className="Continue_with_microsoft">
+              Continue with Microsoft
+            </button>
+          </div>
+
+          <div className="lower_text">
+            <p>
+              Already have an account? <a href="/login">Login</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page; // Export the component as default
